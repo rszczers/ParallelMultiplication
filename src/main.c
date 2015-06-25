d679616d2a518db4b7fd3e222aa891bd055215a0
Rafał Szczerski <rafal.szczerski@gmail.com>
2015-06-26 01:58:03 +0200

    method option

--- src/main.c
+++ src/main.c
@@ -11,60 +11,60 @@ static char doc[] = "Parallel matrix multiplication with Intel Math Kernel libra
 static char args_doc[] = "[FILENAME] [FILENAME]";
 
 static struct argp_option options[] = {
-	{ "method", 'a', "NAME", 0, "Algorithm used."},
+    { "method", 'a', "NAME", 0, "Algorithm used."},
     { "inputA", 'A', "FILE", 0, "Path to input FILE containing matrix A data."},
     { "inputB", 'B', "FILE", 0, "Path to input FILE containing matrix B data."},
     { "output", 'o', "FILE", 0, "Path to output FILE containing matrix C data."},
     { 0, 'm', 0, 0, "m."},
-	{ 0, 'k', 0, 0, "k."},
-	{ 0, 'n', 0, 0, "n."},
+    { 0, 'k', 0, 0, "k."},
+    { 0, 'n', 0, 0, "n."},
     { 0 } 
 };
 
 struct arguments {
-	enum {NAIVE, STRASSEN, CANNON} method;
-	int m, k, n;
-	char *pathA;
-	char *pathB;
-	char *pathC;
+    enum {NAIVE, STRASSEN, CANNON} method;
+    int m, k, n;
+    char *pathA;
+    char *pathB;
+    char *pathC;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-	struct arguments *arguments = state->input;
+    struct arguments *arguments = state->input;
     switch (key) {
-		case 'a':
-		{			
-			char *name = (char *)malloc(8 * sizeof(char));
+        case 'a':
+        {           
+            char *name = (char *)malloc(8 * sizeof(char));
 
-			for (int i = 0; i<9 ; arg++) {
-				name[i] = tolower(*arg);
-				i++;
-			}
+            for (int i = 0; i<9 ; arg++) {
+                name[i] = tolower(*arg);
+                i++;
+            }
 
-			if(strcmp("strassen", name) == 0)
-				arguments->method = STRASSEN;
-			else if(strcmp("cannon", name) == 0)
-				arguments->method = CANNON;
-			// NAIVE is set as default
+            if(strcmp("strassen", name) == 0)
+                arguments->method = STRASSEN;
+            else if(strcmp("cannon", name) == 0)
+                arguments->method = CANNON;
+            // NAIVE is set as default
 
-			printf("%d\n", arguments->method);
-			free(name);			
-			break;
-		}
-		case 'A': {
-			break;
-		}
-		case 'o':
-			break;
-		case 'm':
-			break;
-		case 'n':
-			break;
-		case 'k':
-			break;
+            printf("%d\n", arguments->method);
+            free(name);         
+            break;
+        }
+        case 'A': {
+            break;
+        }
+        case 'o':
+            break;
+        case 'm':
+            break;
+        case 'n':
+            break;
+        case 'k':
+            break;
     case ARGP_KEY_ARG:
-			//	argz_add(&->argz, &a->argz_len, arg);
-			break;	
+            //  argz_add(&->argz, &a->argz_len, arg);
+            break;  
     default: return ARGP_ERR_UNKNOWN;
     }   
     return 0;
@@ -73,16 +73,16 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
 int main(int argc, char *argv[]) {
-	struct arguments arguments;
-	arguments.method = NAIVE;
+    struct arguments arguments;
+    arguments.method = NAIVE;
 
-	argp_parse(&argp, argc, argv, 0, 0, &arguments);
-		
-	//int m = 256;
-	//int n = 256;		
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+        
+    //int m = 256;
+    //int n = 256;      
 
-	//double *out = (double *)malloc(256 * 256 * sizeof(double));
-	//load_matrix("./resources/a.dat"", out, &m, &n);
-	//free(out);
+    //double *out = (double *)malloc(256 * 256 * sizeof(double));
+    //load_matrix("./resources/a.dat"", out, &m, &n);
+    //free(out);
   return 0;
 }
