38d31be683834ad73805a73b51fe250c45dc7245
Rafał Szczerski <rafal.szczerski@gmail.com>
2015-06-26 01:56:08 +0200

    method option

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <argp.h>
+#include <string.h>
 
 #include "load_matrix.h"
 
@@ -10,7 +11,7 @@ static char doc[] = "Parallel matrix multiplication with Intel Math Kernel libra
 static char args_doc[] = "[FILENAME] [FILENAME]";
 
 static struct argp_option options[] = {
-	{ "method", 'a', 0, OPTION_ARG_OPTIONAL, "Algorithm used."},
+	{ "method", 'a', "NAME", 0, "Algorithm used."},
     { "inputA", 'A', "FILE", 0, "Path to input FILE containing matrix A data."},
     { "inputB", 'B', "FILE", 0, "Path to input FILE containing matrix B data."},
     { "output", 'o', "FILE", 0, "Path to output FILE containing matrix C data."},
@@ -29,12 +30,30 @@ struct arguments {
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
     switch (key) {
 		case 'a':
+		{			
+			char *name = (char *)malloc(8 * sizeof(char));
+
+			for (int i = 0; i<9 ; arg++) {
+				name[i] = tolower(*arg);
+				i++;
+			}
+
+			if(strcmp("strassen", name) == 0)
+				arguments->method = STRASSEN;
+			else if(strcmp("cannon", name) == 0)
+				arguments->method = CANNON;
+			// NAIVE is set as default
+
+			printf("%d\n", arguments->method);
+			free(name);			
 			break;
-		case 'i':
-			printf("%s\n", arg);
+		}
+		case 'A': {
 			break;
+		}
 		case 'o':
 			break;
 		case 'm':
@@ -54,8 +73,10 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
 int main(int argc, char *argv[]) {
+	struct arguments arguments;
+	arguments.method = NAIVE;
 
-	argp_parse(&argp, argc, argv, 0, 0, 0);
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
 		
 	//int m = 256;
 	//int n = 256;		
