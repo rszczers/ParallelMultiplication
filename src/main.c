5fcc60d82288ab5bfe9d07bc4a313ea31782d4b1
Rafał Szczerski <rafal.szczerski@gmail.com>
2015-06-26 00:31:28 +0200

    more argp

--- src/main.c
+++ src/main.c
@@ -1,37 +1,51 @@
-#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <argp.h>
-#include <argz.h>
-#include <stdbool.h>
 
 #include "load_matrix.h"
 
-const char *argp_program_version = "pmm 0.1";
+const char *argp_program_version = "pmm v0.1";
 const char *argp_program_bug_address = "<rafal.szczerski@gmail.com>";
-static char doc[] = "Parallel matrix multiplication";
+static char doc[] = "Parallel matrix multiplication with Intel Math Kernel library";
 static char args_doc[] = "[FILENAME] [FILENAME]";
 
-static struct argp_option options[] = { 
-		{ "algorithm", 'a', 0, "OPTION_ARG_OPTIONAL", "Algorithm used."},
-    { "input", 'i', "PATH", 0, "Input matrices: A(m x k), B(k x n)."},
-    { "output", 'o', "PATH", 0, "Path to output file."},
+static struct argp_option options[] = {
+	{ "method", 'a', 0, OPTION_ARG_OPTIONAL, "Algorithm used."},
+    { "inputA", 'A', "FILE", 0, "Path to input FILE containing matrix A data."},
+    { "inputB", 'B', "FILE", 0, "Path to input FILE containing matrix B data."},
+    { "output", 'o', "FILE", 0, "Path to output FILE containing matrix C data."},
     { 0, 'm', 0, 0, "m."},
-		{ 0, 'k', 0, 0, "k."},
-		{ 0, 'n', 0, 0, "n."},
-		{ 0, 'h', 0, OPTION_HIDDEN, 0 },
+	{ 0, 'k', 0, 0, "k."},
+	{ 0, 'n', 0, 0, "n."},
     { 0 } 
 };
 
 struct arguments {
-	char *argz;
-	size_t argz_len;
+	enum {NAIVE, STRASSEN, CANNON} method;
+	int m, k, n;
+	char *pathA;
+	char *pathB;
+	char *pathC;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-    struct arguments *a = state->input;
     switch (key) {
-    case ARGP_KEY_ARG: return 0;
+		case 'a':
+			break;
+		case 'i':
+			printf("%s\n", arg);
+			break;
+		case 'o':
+			break;
+		case 'm':
+			break;
+		case 'n':
+			break;
+		case 'k':
+			break;
+    case ARGP_KEY_ARG:
+			//	argz_add(&->argz, &a->argz_len, arg);
+			break;	
     default: return ARGP_ERR_UNKNOWN;
     }   
     return 0;
@@ -41,16 +55,13 @@ static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
 int main(int argc, char *argv[]) {
 
-	argp_parse(&argp, argc, argv, 0, 0, &arguments);
-	
-	int m = 256;
-	int n = 256;		
-	if(argc >= 2) {
-		printf("%s\n", argv[1]);
-	}
+	argp_parse(&argp, argc, argv, 0, 0, 0);
+		
+	//int m = 256;
+	//int n = 256;		
 
-	double *out = (double *)malloc(256 * 256 * sizeof(double));
-    load_matrix("./resources/a.dat", out, &m, &n);
-	free(out);
-    return 0;
+	//double *out = (double *)malloc(256 * 256 * sizeof(double));
+	//load_matrix("./resources/a.dat"", out, &m, &n);
+	//free(out);
+  return 0;
 }
