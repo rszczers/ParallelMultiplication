81b648e6b6478d18aa0663a1837f9023d2d3d700
Rafał Szczerski <rafal.szczerski@gmail.com>
2015-09-08 22:15:49 +0200

    appendices

--- job.sh
+++ job.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #PBS -N pmm
-#PBS -l nodes=14:ppn=1,mem=8GB,walltime=04:00:00:00
+#PBS -l nodes=32:ppn=1
+#PBS -l walltime=72:00:00
 #PBS -q default 
 #PBS -m abe
 #PBS -M rafal.szczerski@gmail.com
 #PBS -o pmm.log
-cd $PBS_O_WORKDIR
 
+cd $PBS_O_WORKDIR
 for s in 2048 4096 8192; do
     make data SIZE=$s
     echo "make data SIZE=$s"
-    sleep 1
     make test SIZE=$s
-    mv ./debug/debug_* ./debug/tmp/
 done
-
-
--- job_seq.sh
+++ job_seq.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #PBS -N pmm_seqpart
-#PBS -l nodes=8,walltime=:04:00:00
+#PBS -l nodes=8
+#PBS -l walltime=04:00:00
 #PBS -q default 
 #PBS -m abe
 #PBS -M rafal.szczerski@gmail.com
 #PBS -o pmm.log
-cd $PBS_O_WORKDIR
 
+cd $PBS_O_WORKDIR
 make data SIZE=4096 
 echo "make data SIZE=4096"
-sleep 1
 ./makeplots_seq.sh 384 4096
+mv ./debug/debug_* ./debug/seq/
